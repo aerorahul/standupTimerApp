2dc437cee310660e78f135fa505b17a3d6783a64
Rahul Mahajan <aerorahul@users.noreply.github.com>
2025-10-31 14:41:41 -0400

    add google analytics tag to webapp

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-02J51VNVCE"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-02J51VNVCE');
+</script>
